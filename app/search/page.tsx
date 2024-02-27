3e051a94e3ecda2133983781c8a02868c46b1dcc
shpetimaliu <shpetimmaliu@gmail.com>
2024-02-27 13:51:39 +0100

    I uploaded a json data product, since the api was free and often crashed" ;

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,4 @@
-import { FetchSearch } from "@/lib/fetchSearch";
+import products from "../../public/pr.json";
 
 type Props = {
   searchParams: {
@@ -7,23 +7,20 @@ type Props = {
 };
 
 async function SearchPage({ searchParams: { q } }: Props) {
-  console.log(q);
+  console.log(products);
+  if (!products.search_results) {
+    return <div>No search results found.</div>;
+  }
 
-  const results = await FetchSearch(q);
-
-  console.log(results);
   return (
     <div className="p-10">
-      <h1 className="text-3xl font-bold mb-2">Result for {q}</h1>
+      <h1 className="text-3xl font-bold mb-2">Result for: {q}</h1>
       <h2 className="mb-5 text-gray-200"></h2>
 
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-        (
-        {results.search_results &&
-          results.search_results.product.map((product: any) => (
-            <li key={product.product_id}>{product.title}</li>
-          ))}
-        results)
+        {products.search_results.map((item: any, index: number) => (
+          <li key={index}>{item.product.title}</li>
+        ))}
       </ul>
     </div>
   );
